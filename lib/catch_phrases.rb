b3a9cd53546e468b85cfd780f6861dfbb59c8824
Payton Gray <paytongray18@gmail.com>
2020-07-07 15:56:33 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -1,7 +1,6 @@
-$status = 
+$status = 'Thank You Mario! But Our Princess Is In Another Castle!'
 
 def mario
-  status = 'Thank You Mario! But Our Princess Is In Another Castle!'
   phrase = "It's-a me, Mario!"
   puts phrase
 end
